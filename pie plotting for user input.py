a9814a3ff4215fad75eef810acdbe3b7a48e7635
HARI SARAN <harisaran479@gmail.com>
2026-03-19 21:03:13 +0530

    Create pie plotting for user input.py

---
+++ pie plotting for user input.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+n = int(input("Enter number of categories: "))
+labels = []
+sizes = []
+for i in range(n):
+    label = input(f"Enter label for category {i+1}: ")
+    value = float(input(f"Enter value for {label}: "))
+    
+    labels.append(label)
+    sizes.append(value)
+
+plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+plt.axis('equal')
+plt.title("User Input Pie Chart")
+plt.show()
