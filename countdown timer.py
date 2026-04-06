bab19f420832a5a4dea225f8d9ccffc89f91e78c
HARI SARAN <harisaran479@gmail.com>
2026-04-06 20:39:28 +0530

    Correct timer display in countdown timer
    Fix countdown timer display to show time correctly.

--- countdown timer.py
+++ countdown timer.py
@@ -1,11 +1,9 @@
 import time
-
 seconds = int(input("Enter time in seconds: "))
-
 while seconds:
     mins, secs = divmod(seconds, 60)
-    timer = f"{mins:02d}:{secs:02d}"
-    print(timer, end="\r")
+    timer = (f"{mins:02d}:{secs:02d}")
+    print(timer)
     time.sleep(1)
     seconds -= 1
 
