e21a70275cc55deda80e881997a49226acf5126a
HARI SARAN <harisaran479@gmail.com>
2026-04-06 20:30:41 +0530

    Add countdown timer functionality

---
+++ countdown timer.py
@@ -0,0 +1,12 @@
+import time
+
+seconds = int(input("Enter time in seconds: "))
+
+while seconds:
+    mins, secs = divmod(seconds, 60)
+    timer = f"{mins:02d}:{secs:02d}"
+    print(timer, end="\r")
+    time.sleep(1)
+    seconds -= 1
+
+print("Time's up!")
