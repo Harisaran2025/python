bfd39e29ba611fb1c8deca04023a2dce1917e13d
HARI SARAN <harisaran479@gmail.com>
2026-05-15 14:01:26 +0530

    Implement match case for days of the week

---
+++ match case in python.py
@@ -0,0 +1,18 @@
+day = int(input("Enter the case from 1 to 9:"))
+match day:
+  case 1:
+    print("Monday")
+  case 2:
+    print("Tuesday")
+  case 3:
+    print("Wednesday")
+  case 4:
+    print("Thursday")
+  case 5:
+    print("Friday")
+  case 6:
+    print("Saturday")
+  case 7:
+    print("Sunday")
+  case 8 | 9 :
+    print("Saturday and sunday are weekends")
